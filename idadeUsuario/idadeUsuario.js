2169ab9165c1b867c550275cf0037092dabaf2fd
JoelmaCamargo <joelmavilela023@gmail.com>
2021-04-03 11:21:15 -0300

    Primeiro exercício - idadeUsuario

---
+++ idadeUsuario/idadeUsuario.js
@@ -0,0 +1,11 @@
+var nome = "Joelma"
+var idade = 27
+
+alert("Olá, meu nome é " + nome + ", tenho " + idade +" anos. E esse é meu primeiro código javascript!")
+var idadeUsuario = prompt("Quantos anos você tem?")
+var idadeUsuarioComoNumero = parseInt(idadeUsuario)
+
+alert("Sua idade no ano que vem será exibida a seguir")
+idadeUsuarioComoNumero = idadeUsuarioComoNumero + 1
+
+alert(idadeUsuarioComoNumero + " anos")
