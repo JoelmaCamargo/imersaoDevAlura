38ab8eea873117fc3a99acada85babcda788cadd
Joelma Camargo <joelmavilela023@gmail.com>
2023-02-08 12:23:29 -0300

    Exercício - Conversor de Moedas

---
+++ imersaoDevAlura/conversorDeMoedas/.js
@@ -0,0 +1,8 @@
+alert('Qual o valor em dólar você quer converter?');
+
+var valorEmDolarNumero = parseFloat(valorEmDolarTexto);
+
+var valorEmReal = valorEmDolarNumero * 5.15;
+var valorEmRealFixado = valorEmReal.toFixed(2);
+
+alert("R$ " + valorEmRealFixado);
